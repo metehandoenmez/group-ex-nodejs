c8e068d1da0a72c14b7179741e8ca03fddfc1ed4
metehandoenmez <metehandoenmez@gmail.com>
2023-07-03 14:30:38 +0300

    completed exercise

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,34 @@
-import express from "express";
+import express, { response } from "express";
+import { request } from "http";
 
 const app = express();
 const port = 3000;
+
+let var1 = "string";
+
+app.use(express.json());
+
+app.get("/api/variable", (request, response) => {
+  response.json(var1);
+});
+
+app.post("/api/variable", (request, response) => {
+  const { value } = request.body;
+  var1 = value;
+  response.json(var1);
+});
+
+app.delete("/api/variable", (request, response) => {
+  var1 = "";
+  response.json({ status: "success" });
+});
+
+app.put("/api/variable", (request, response) => {
+  const { value } = request.body;
+  var1 = value;
+  response.json(var1);
+});
+
+app.listen(port, () => {
+  console.log(`Server Started on port ${port}`);
+});
